a873a46107778acbb91373e47ba6e34a2a95f5a0
Danny Caulwell <caulwell@protonmail.com>
2022-05-10 23:43:58 +0100

    Display Products In DataTable

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -20,8 +20,7 @@ namespace BulkyBookWeb.Controllers;
         }
         public IActionResult Index()
         {
-            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
-            return View(objProductList);
+            return View();
         }
 
         //GET
@@ -46,45 +45,46 @@ namespace BulkyBookWeb.Controllers;
         if (id == null || id == 0)
         {
             return View(productVM);
-        }else
-        {
-            //update product
         }
-
-        return View(productVM);
-        }
-
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Upsert(ProductVM obj, IFormFile file)
+        else
         {
-            if (ModelState.IsValid)
-            {
+            productVM.Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+            return View(productVM);
+        }        
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Upsert(ProductVM obj, IFormFile? file)
+    {
+
+        if (ModelState.IsValid)
+        {
             string wwwRootPath = _hostEnvironment.WebRootPath;
-            if(file != null)
+            if (file != null)
             {
                 string fileName = Guid.NewGuid().ToString();
                 var uploads = Path.Combine(wwwRootPath, @"images\products");
                 var extension = Path.GetExtension(file.FileName);
 
-                using (var fileStreams = new FileStream(Path.Combine(uploads, fileName+extension), FileMode.Create))
+                using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
                 {
                     file.CopyTo(fileStreams);
                 }
                 obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
-            }
-                _unitOfWork.Product.Add(obj.Product);
-                _unitOfWork.Save();
-                TempData["success"] = "Product created successfully";
-                return RedirectToAction("Index");
-            }
-            return View(obj);
 
+            }
+            _unitOfWork.Product.Add(obj.Product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product created successfully";
+            return RedirectToAction("Index");
         }
+        return View(obj);
+    }
 
-        //GET
-        public IActionResult Delete(int? id)
+    //GET
+    public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
             {
@@ -115,5 +115,14 @@ namespace BulkyBookWeb.Controllers;
             return RedirectToAction("Index");
 
         }
+    #region API CALLS
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+        return Json(new { data = productList });
     }
+    #endregion
+
+}
 
---
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -0,0 +1,34 @@
+﻿var datatable;
+
+$(document).ready(() => {
+    loadDataTable();
+});
+
+const loadDataTable = () => {
+    dataTable = $("#tblData").DataTable({
+        "ajax": {
+            "url":"/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            { "data": "author", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": data => {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a class="btn btn-primary mx-2" href="/Admin/Product/Upsert?id=${data}"><i class="bi bi-pencil-square"></i>Edit</a>
+                            <a class="btn btn-danger mx-2" href="/Admin/Product/Delete?id=${data}"><i class="bi bi-trash-fill"></i>Delete</a>
+                        </div>
+                    `;
+                },
+                "width": "15%"
+            }
+
+
+        ]
+    });
+};
