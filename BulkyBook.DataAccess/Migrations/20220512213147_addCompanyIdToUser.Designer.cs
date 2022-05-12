2c7e4a4247867edae57885dd46f658c1f5245df3
Danny Caulwell <caulwell@protonmail.com>
2022-05-12 22:52:47 +0100

    allow choose company on register

--- BulkyBook.DataAccess/Migrations/20220512213147_addCompanyIdToUser.Designer.cs
+++ BulkyBook.DataAccess/Migrations/20220512213147_addCompanyIdToUser.Designer.cs
@@ -4,16 +4,18 @@ using BulkyBook.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace BulkyBookWeb.Migrations
+namespace BulkyBook.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220512213147_addCompanyIdToUser")]
+    partial class addCompanyIdToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
