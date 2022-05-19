28ae4e875be368a314070c8cbc136486a313b28b
Danny Caulwell <caulwell@protonmail.com>
2022-05-19 16:03:06 +0100

    Allow update order details

--- BulkyBook.DataAccess/Repository/Repository.cs
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -39,9 +39,10 @@ namespace BulkyBook.DataAccess.Repository
             return query.ToList();
         }
 
-        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked=true)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+            query = tracked ? dbSet : dbSet.AsNoTracking();
             query = query.Where(filter);
             if (includeProperties != null)
             {
